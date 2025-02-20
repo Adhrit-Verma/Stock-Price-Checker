30534005ae064c41f483558ffda484ec42889d5d
Kafy Dier <works.omen@gmail.com>
2025-02-20 14:04:01 +0530

    updated a lot

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 const dbPath = process.env.DB_PATH || './portfolio.db';
 
 // Middleware to parse JSON and URL-encoded bodies
